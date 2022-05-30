04ac26ed61fedd59de52f4067e5556cef0a026b5
Nobuo Yamashita <nyama@seitoku.ac.jp>
2022-05-30 14:19:21 +0900

    update 2022-05-30

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,10 @@ module Main where
 
 import System.Environment ( getArgs )
 import Data.List.Extra
-import Transition ( responser )
+import Wordle ( wordle )
 import Interact ( interactWithPrompt )
 
 main :: IO ()
-main = interactWithPrompt "? " ":quit"
-     . responser =<< list (readFile "/usr/share/dict/words") (const . readFile)
+main = interactWithPrompt "? " ":bye"
+     . wordle =<< list (readFile "/usr/share/dict/words") (const . readFile)
                  =<< getArgs
